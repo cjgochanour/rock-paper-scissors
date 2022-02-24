3149356b9468870764881bedb3705563ba914e5a
cjgochanour <cjgochanour@gmail.com>
2022-02-24 16:18:15 -0600

    Player is asked what they would like to throw and are told what number they selected

--- Program.cs
+++ Program.cs
@@ -6,3 +6,12 @@ int ComputerScore = 0;
 Console.WriteLine("-----------------------------");
 Console.WriteLine($"| Player: {PlayerScore}  |  Computer: {ComputerScore} |");
 Console.WriteLine("-----------------------------");
+
+Console.WriteLine("What would you like to throw?");
+Console.WriteLine("1) Rock");
+Console.WriteLine("2) Paper");
+Console.WriteLine("3) Scissors");
+
+int PlayerChoice = int.Parse (Console.ReadLine());
+
+Console.WriteLine($"You chose number {PlayerChoice}");

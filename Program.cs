84ae262965af0f11291f49f2ef28e381ab233c65
cjgochanour <cjgochanour@gmail.com>
2022-02-24 17:51:35 -0600

    added win conditions, game continues until game is won

--- Program.cs
+++ Program.cs
@@ -4,18 +4,6 @@ using System.Collections.Generic;
 int PlayerScore = 0;
 int ComputerScore = 0;
 
-Console.WriteLine(@$"-----------------------------
-| Player: {PlayerScore}  |  Computer: {ComputerScore} |
------------------------------");
-
-Console.WriteLine("What would you like to throw?");
-Console.WriteLine("1) Rock");
-Console.WriteLine("2) Paper");
-Console.WriteLine("3) Scissors");
-
-int PlayerChoice = int.Parse (Console.ReadLine()) - 1;
-int ComputerChoice =new Random().Next(0, 3);
-
 List<string> Throws = new List<string>() {
     // thanks to wynand1004 for the art!;
     @"    _______
@@ -38,8 +26,34 @@ List<string> Throws = new List<string>() {
 ---.__(___)"
 };
 
-Console.WriteLine($@"{Throws[PlayerChoice]}
+main();
+
+void main()
+{
+    while (PlayerScore != 3 || ComputerScore != 3)
+    {
+        Console.WriteLine(@$"-----------------------------
+| Player: {PlayerScore}  |  Computer: {ComputerScore} |
+-----------------------------");
+
+        Console.WriteLine("What would you like to throw?");
+        Console.WriteLine("1) Rock");
+        Console.WriteLine("2) Paper");
+        Console.WriteLine("3) Scissors");
+        int PlayerChoice = int.Parse(Console.ReadLine()) - 1;
+        int ComputerChoice = new Random().Next(0, 3);
+        Console.WriteLine($@"{Throws[PlayerChoice]}
 
 vs
 
 {Throws[ComputerChoice]}");
+    }
+    if (PlayerScore == 3)
+    {
+        Console.WriteLine("YOU WIN!");
+    }
+    else
+    {
+        Console.WriteLine("YOU LOSE!");
+    }
+};
